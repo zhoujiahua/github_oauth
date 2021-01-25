b5724e7ec4a2e6a89cb123714aaaabe0e0ab7d68
jerryZhou <jerry.zhou@ptn.advantest.com>
2021-01-25 16:23:17 +0800

    Add app floder

---
+++ app/a.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+# coding: utf-8
+
+print('ok')
+a = 3.1415926543448489
+print(round(a,3))
--- basic/c1.py
+++ basic/c1.py
@@ -1,7 +1,3 @@
 #! /usr/bin/python3
-<<<<<<< HEAD
-a = 7
-=======
 a = 7
 print(a)
->>>>>>> a78c47a... update code
